2bdccd56acca5a2bbb59330ea2e2fe3b6e564e95
KIMSEI1124 <workju1124@gmail.com>
2023-08-31 09:17:31 +0900

    Init: Start Project
    Java 11
    Spring Boot 2.7.15

---
+++ src/main/java/com/ssafy/tott/TottApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.tott;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TottApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TottApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssafy/tott/TottApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.tott;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TottApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
