839d1478b413a4c2efd98b1b0dff440944e06b8c
KIMSEI1124 <workju1124@gmail.com>
2023-09-01 13:15:13 +0900

    Feat: 각 도메인 별 Repository 생성
    Resolver: #3

---
+++ src/main/java/com/ssafy/tott/housedetail/domain/HouseDetailRepository.java
@@ -0,0 +1,6 @@
+package com.ssafy.tott.housedetail.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HouseDetailRepository extends JpaRepository<HouseDetail, Integer> {
+}
