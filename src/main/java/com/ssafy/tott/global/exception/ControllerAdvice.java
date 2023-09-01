a0101c146db679d17d9e31a4c30504c56ca14153
KIMSEI1124 <workju1124@gmail.com>
2023-09-02 00:59:18 +0900

    Feat: ControllerAdvice 구현
    ExceptionResponse Dto를 생성하여 예외가 발생하였을 때 반환
    ExceptionHandler로 TOTTException 이 던져지면 진행상황을 가로채서 반환

---
+++ src/main/java/com/ssafy/tott/global/exception/ControllerAdvice.java
@@ -0,0 +1,14 @@
+package com.ssafy.tott.global.exception;
+
+import com.ssafy.tott.global.exception.dto.response.ExceptionResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerAdvice {
+    @ExceptionHandler(TOTTException.class)
+    public ResponseEntity<ExceptionResponse> handleException(TOTTException e) {
+        return ResponseEntity.status(e.getStatusCode()).body(ExceptionResponse.from(e));
+    }
+}
---
+++ src/main/java/com/ssafy/tott/global/exception/dto/response/ExceptionResponse.java
@@ -0,0 +1,20 @@
+package com.ssafy.tott.global.exception.dto.response;
+
+import com.ssafy.tott.global.exception.TOTTException;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Data
+public class ExceptionResponse {
+    private String message;
+
+    private ExceptionResponse(String message) {
+        this.message = message;
+    }
+
+    public static ExceptionResponse from(TOTTException e) {
+        return new ExceptionResponse(e.getMessage());
+    }
+}
