48e3832edc733666cc38cc471ae544f746bd1874
KIMSEI1124 <workju1124@gmail.com>
2023-09-02 10:32:22 +0900

    Feat: 이메일 검증 기능 구현
    입력된 이메일이 유효하지 않으면 예외를 던짐
    추후 수정할 부분이 있어 TODO로 남겨둠

--- src/main/java/com/ssafy/tott/member/domain/embbeded/Email.java
+++ src/main/java/com/ssafy/tott/member/domain/embbeded/Email.java
@@ -1,13 +1,20 @@
 package com.ssafy.tott.member.domain.embbeded;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import java.util.regex.Pattern;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Embeddable
 public class Email {
+    private static final String EMAIL_FORMAT = "^[a-z0-9._-]{8,16}+@[a-z]+[.]+[a-z]{2,3}$";
+    private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_FORMAT);
+
     @Column(name = "email", unique = true, nullable = false)
     private String value;
 
@@ -16,6 +23,18 @@ public class Email {
     }
 
     public static Email from(String email) {
+        validateEmailFormat(email);
         return new Email(email);
+    }
+
+    private static void validateEmailFormat(String email) {
+        if (isNotValidEmail(email)) {
+            /* TODO: 2023/09/02 추후 `Custom Exception`으로 수정 */
+            throw new RuntimeException();
+        }
+    }
+
+    private static boolean isNotValidEmail(String email) {
+        return !EMAIL_PATTERN.matcher(email).matches();
     }
 }
