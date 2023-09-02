a312ff3b1078d070bcf0d5cd352c2ec3b9fd2abe
KIMSEI1124 <workju1124@gmail.com>
2023-09-02 10:43:21 +0900

    Feat: 비밀번호 생성자 생성
    기본 생성자를 protected 로 제한

--- src/main/java/com/ssafy/tott/member/domain/embbeded/Password.java
+++ src/main/java/com/ssafy/tott/member/domain/embbeded/Password.java
@@ -1,13 +1,24 @@
 package com.ssafy.tott.member.domain.embbeded;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
 @Getter
 @Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Password {
     @Column(name = "password", nullable = false)
     private String value;
+
+    private Password(String password) {
+        value = password;
+    }
+
+    public static Password from(String password) {
+        return new Password(password);
+    }
 }
