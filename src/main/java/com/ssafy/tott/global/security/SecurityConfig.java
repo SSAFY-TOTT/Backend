fbbd6ee9aa2e8c4a46a26607aaeaa427443a0abf
KIMSEI1124 <workju1124@gmail.com>
2023-09-06 08:32:48 +0900

    Feat: SecurityConfig 작성

---
+++ src/main/java/com/ssafy/tott/global/security/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.ssafy.tott.global.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .cors()
+                /* TODO: 2023/09/06 401, 403 예외 처리 구현 */
+
+                .and()
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .authorizeRequests()
+                /* TODO: 2023/09/06 추후 로그인 유저만 사용 가능한 API 제작 */
+                .anyRequest().permitAll();
+
+        return http.build();
+    }
+}
