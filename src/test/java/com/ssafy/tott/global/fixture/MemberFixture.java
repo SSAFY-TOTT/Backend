1b9c3644b6ce1f604a3e356814aff1ca7b98d155
KIMSEI1124 <workju1124@gmail.com>
2023-09-06 09:39:37 +0900

    Test: 로그인 테스트 코드 작성

--- src/test/java/com/ssafy/tott/global/fixture/MemberFixture.java
+++ src/test/java/com/ssafy/tott/global/fixture/MemberFixture.java
@@ -1,6 +1,7 @@
 package com.ssafy.tott.global.fixture;
 
 import com.ssafy.tott.account.domain.BankCode;
+import com.ssafy.tott.auth.dto.request.LoginRequest;
 import com.ssafy.tott.member.domain.Member;
 import com.ssafy.tott.member.domain.embbeded.Email;
 import com.ssafy.tott.member.domain.embbeded.Password;
@@ -44,6 +45,13 @@ public enum MemberFixture {
                 .password(Password.of(password, new BCryptPasswordEncoder()))
                 .creditLine(0L)
                 .phoneNumber(PhoneNumber.from(phoneNumber))
+                .build();
+    }
+
+    public LoginRequest toLoginRequest() {
+        return LoginRequest.builder()
+                .email(email)
+                .password(password)
                 .build();
     }
 }
