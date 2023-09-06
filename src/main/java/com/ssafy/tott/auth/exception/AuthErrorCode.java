2d8e6ed1b83a5ab4361f923c8a2af74d9dd95ad6
KIMSEI1124 <workju1124@gmail.com>
2023-09-06 10:11:58 +0900

    Feat: 401, 403 예외처리 구현

--- src/main/java/com/ssafy/tott/auth/exception/AuthErrorCode.java
+++ src/main/java/com/ssafy/tott/auth/exception/AuthErrorCode.java
@@ -9,7 +9,9 @@ public enum AuthErrorCode implements ErrorCode {
     ERROR_CLIENT_BY_JWT_SIGNATURE_INVALID(401, "AUTH_02", "잘못된 서명입니다."),
     ERROR_CLINET_BY_JWT_KEY_EXPIERD(401, "AUTH_03", "만료된 토큰입니다."),
     ERROR_CLIENT_BY_JWT_NOT_SUPPORT(401, "AUTH_04", "지원하지 않는 토큰입니다."),
-    ERROR_CLIENT_BY_JWT_KEY_INVALID(401, "AUTH_05", "잘못된 토큰입니다.")
+    ERROR_CLIENT_BY_JWT_KEY_INVALID(401, "AUTH_05", "잘못된 토큰입니다."),
+    ERROR_CLIENT_BY_AUTH_PERMISSION_TO_ACCESS_THE_REQUEST_ROLE(403, "AUTH_06", "해당 기능에 대한 권한이 없습니다."),
+    ERROR_CLIENT_BY_AUTHENTICATED_MUST_BE_VALID(401, "AUTH_07", "사용자 인증이 필요합니다."),
     ;
     private int statusCode;
     private String errorCode;
--- src/main/java/com/ssafy/tott/global/security/SecurityConfig.java
+++ src/main/java/com/ssafy/tott/global/security/SecurityConfig.java
@@ -1,5 +1,10 @@
 package com.ssafy.tott.global.security;
 
+import com.ssafy.tott.auth.support.JwtAccessDeniedHandler;
+import com.ssafy.tott.auth.support.JwtAuthenticationEntryPoint;
+import com.ssafy.tott.auth.support.JwtSecurityConfig;
+import com.ssafy.tott.auth.support.TokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,8 +13,13 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+@RequiredArgsConstructor
 @Configuration
 public class SecurityConfig {
+
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final TokenProvider tokenProvider;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -19,8 +29,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .cors()
-                /* TODO: 2023/09/06 401, 403 예외 처리 구현 */
+                .cors().and()
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
 
                 .and()
                 .headers()
@@ -34,7 +46,10 @@ public class SecurityConfig {
                 .and()
                 .authorizeRequests()
                 /* TODO: 2023/09/06 추후 로그인 유저만 사용 가능한 API 제작 */
-                .anyRequest().permitAll();
+                .anyRequest().permitAll()
+
+                .and()
+                .apply(new JwtSecurityConfig(tokenProvider));
 
         return http.build();
     }
