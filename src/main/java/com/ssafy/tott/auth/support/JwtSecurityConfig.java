2e78531ea2cbcb1be1c4151c388084f2b776debd
KIMSEI1124 <workju1124@gmail.com>
2023-09-06 10:16:45 +0900

    Feat: JwtSecurityConfig 작성

--- src/main/java/com/ssafy/tott/auth/support/JwtSecurityConfig.java
+++ src/main/java/com/ssafy/tott/auth/support/JwtSecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @RequiredArgsConstructor
 public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
@@ -11,6 +12,7 @@ public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurity
 
     @Override
     public void configure(HttpSecurity builder) throws Exception {
-
+        JwtFilter customFilter = new JwtFilter(tokenProvider);
+        builder.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class);
     }
 }
