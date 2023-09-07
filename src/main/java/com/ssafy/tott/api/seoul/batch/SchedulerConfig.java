a43cfd2cd25c107bd499b724799ebec7137c8075
Kyongmin1016 <rkdrnwkd1@gmail.com>
2023-09-07 11:05:39 +0900

    SchedulerConfig schedule 임시수정

--- src/main/java/com/ssafy/tott/api/seoul/batch/SchedulerConfig.java
+++ src/main/java/com/ssafy/tott/api/seoul/batch/SchedulerConfig.java
@@ -12,7 +12,9 @@ import org.springframework.scheduling.annotation.Scheduled;
 public class SchedulerConfig {
     private final SchedulerService schedulerService;
 
-    @Scheduled(cron = "${schedules.cron.reward.publish}")/* 01:00 Start*/
+
+//    @Scheduled(cron = "${schedules.cron.reward.publish}")/* 01:00 Start*/
+    @Scheduled(fixedDelay = 60*60*24,initialDelay = 3000)   /* 3초후 시작, 24시간 후 업데이트 */
     void runHouseDataJob() {
         schedulerService.fetchHouseData(100);
     }
