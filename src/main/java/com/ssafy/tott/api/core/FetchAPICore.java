b295eb9719345059f3f0c7f4c3465f545df049be
KIMSEI1124 <workju1124@gmail.com>
2023-09-10 21:33:01 +0900

    Feat: APIRequest 인터페이스 생성

--- src/main/java/com/ssafy/tott/api/core/FetchAPICore.java
+++ src/main/java/com/ssafy/tott/api/core/FetchAPICore.java
@@ -1,5 +1,7 @@
 package com.ssafy.tott.api.core;
 
+import com.ssafy.tott.api.core.dto.APIResponse;
+
 public interface FetchAPICore {
     default APIResponse fetchAPI(String request) {
         return null;
--- src/main/java/com/ssafy/tott/api/seoulopendata/data/dto/response/RentAPIResponse.java
+++ src/main/java/com/ssafy/tott/api/seoulopendata/data/dto/response/RentAPIResponse.java
@@ -2,7 +2,7 @@ package com.ssafy.tott.api.seoulopendata.data.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.ssafy.tott.api.core.APIResponse;
+import com.ssafy.tott.api.core.dto.APIResponse;
 import com.ssafy.tott.api.seoulopendata.data.vo.RentData;
 import lombok.Getter;
 import lombok.ToString;
