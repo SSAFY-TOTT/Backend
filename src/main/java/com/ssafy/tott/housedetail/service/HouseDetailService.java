9dc719a4f58d2b5c704b0df5bfdb43c875c6b3b8
KIMSEI1124 <workju1124@gmail.com>
2023-09-12 08:25:49 +0900

    Feat: RecentView Service 함수 작성
    Repository 쿼리문이 작성되면 세부 구현필요

--- src/main/java/com/ssafy/tott/housedetail/service/HouseDetailService.java
+++ src/main/java/com/ssafy/tott/housedetail/service/HouseDetailService.java
@@ -1,17 +1,27 @@
 package com.ssafy.tott.housedetail.service;
 
 import com.ssafy.tott.api.seoulopendata.data.vo.RentRow;
+import com.ssafy.tott.auth.vo.AuthMember;
+import com.ssafy.tott.housedetail.data.dto.request.HouseDetailRecentViewRequest;
+import com.ssafy.tott.housedetail.data.vo.HouseDetailRecentViewVO;
 import com.ssafy.tott.housedetail.domain.HouseDetailRepository;
 import com.ssafy.tott.housedetail.mapper.HouseDetailMapper;
 import com.ssafy.tott.housegeo.domain.HouseGeo;
+import com.ssafy.tott.member.domain.Member;
+import com.ssafy.tott.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class HouseDetailService {
     private final HouseDetailRepository houseDetailRepository;
     private final HouseDetailMapper houseDetailMapper;
+
+    private final MemberService memberService;
 
     /**
      * HouseDetail 데이터를 저장한다.
@@ -21,5 +31,13 @@ public class HouseDetailService {
      */
     public void saveHouseDetail(RentRow row, HouseGeo houseGeo) {
         houseDetailRepository.save(houseDetailMapper.toEntity(row, houseGeo));
+    }
+
+    public List<HouseDetailRecentViewVO> findByRecentView(
+            AuthMember authMember, HouseDetailRecentViewRequest request) {
+        Member findMember = memberService.findById(authMember.getMemberId());
+        List<HouseDetailRecentViewVO> list = new ArrayList<>();
+
+        return list;
     }
 }
