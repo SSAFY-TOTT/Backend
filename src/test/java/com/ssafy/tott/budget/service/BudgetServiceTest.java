e035580430914020d105d62bf5aa92e434660be3
KIMSEI1124 <workju1124@gmail.com>
2023-09-13 09:30:54 +0900

    Refactor: BudgetService 수정
    BDDMockito에서 다른 테스트로 수정

--- src/test/java/com/ssafy/tott/budget/service/BudgetServiceTest.java
+++ src/test/java/com/ssafy/tott/budget/service/BudgetServiceTest.java
@@ -4,20 +4,14 @@ import com.ssafy.tott.auth.vo.AuthMember;
 import com.ssafy.tott.budget.data.dto.requset.BudgetsUpdateRequest;
 import com.ssafy.tott.budget.data.dto.response.BudgetsResponse;
 import com.ssafy.tott.budget.data.vo.BudgetVO;
-import com.ssafy.tott.budget.domain.Budget;
-import com.ssafy.tott.budget.domain.BudgetRepository;
 import com.ssafy.tott.budget.fixture.BudgetFixture;
+import com.ssafy.tott.global.config.ServiceTest;
 import com.ssafy.tott.global.fixture.MemberFixture;
 import com.ssafy.tott.member.domain.Member;
-import com.ssafy.tott.member.service.MemberService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,27 +19,18 @@ import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.mockito.ArgumentMatchers.anyCollection;
-import static org.mockito.BDDMockito.given;
 
-@ActiveProfiles("test")
-@ExtendWith(MockitoExtension.class)
-class BudgetServiceTest {
+class BudgetServiceTest extends ServiceTest {
 
-    @InjectMocks
+    @Autowired
     private BudgetService budgetService;
-    @Mock
-    private MemberService memberService;
-    @Mock
-    private BudgetRepository budgetRepository;
     private Member savedMember;
     private AuthMember authMember;
 
     @BeforeEach
     void setUp() {
-        savedMember = MemberFixture.SHINHAN.toMemberWithId(1);
+        savedMember = saveMember(MemberFixture.SHINHAN);
         authMember = new AuthMember(savedMember.getId());
-        given(memberService.findById(savedMember.getId())).willReturn(savedMember);
     }
 
     @DisplayName("추가 예산을 정상적으로 수정한다.")
@@ -53,7 +38,6 @@ class BudgetServiceTest {
     void updateAllTest() {
         /* Given */
         BudgetsUpdateRequest request = toRequest(savedMember);
-        given(budgetRepository.saveAll(anyCollection())).willReturn(toBudgets());
 
         /* When */
         BudgetsResponse response = budgetService.saveAll(authMember, request);
@@ -61,14 +45,15 @@ class BudgetServiceTest {
         /* Then */
         assertAll(
                 () -> assertThat(response).isNotNull(),
-                () -> assertThat(Objects.requireNonNull(response).getBudgets()).hasSize(2));
+                () -> assertThat(Objects.requireNonNull(response).getBudgets()).hasSize(2)
+        );
     }
 
     @DisplayName("예산 목록을 정상적으로 조회한다.")
     @Test
     void findAllTest() {
         /* Given */
-        given(budgetRepository.findByMember(savedMember)).willReturn(toBudgets());
+        budgetService.saveAll(authMember, toRequest(savedMember));
 
         /* When */
         BudgetsResponse response = budgetService.findAll(authMember);
@@ -76,26 +61,13 @@ class BudgetServiceTest {
         /* Then */
         assertAll(
                 () -> assertThat(response).isNotNull(),
-                () -> assertThat(Objects.requireNonNull(response).getBudgets()).hasSize(2));
+                () -> assertThat(Objects.requireNonNull(response).getBudgets()).hasSize(2)
+        );
     }
 
     private BudgetsUpdateRequest toRequest(Member member) {
-        return new BudgetsUpdateRequest(
-                new ArrayList<>(
-                        List.of(
-                                BudgetVO.from(BudgetFixture.TEN_MILLION_WON.toBudget(member)),
-                                BudgetVO.from(BudgetFixture.ONE_MILLION_WON.toBudget(member)))));
-    }
-
-    private List<Budget> toBudgets() {
-        int sequence = 1;
-        return new ArrayList<>(
-                List.of(
-                        toBudget(sequence++, savedMember, BudgetFixture.ONE_MILLION_WON),
-                        toBudget(sequence, savedMember, BudgetFixture.TEN_MILLION_WON)));
-    }
-
-    private Budget toBudget(int sequence, Member member, BudgetFixture budgetFixture) {
-        return budgetFixture.toBudgetWithId(sequence, member);
+        return new BudgetsUpdateRequest(new ArrayList<>(List.of(
+                BudgetVO.from(BudgetFixture.TEN_MILLION_WON.toBudget(member)),
+                BudgetVO.from(BudgetFixture.ONE_MILLION_WON.toBudget(member)))));
     }
 }
