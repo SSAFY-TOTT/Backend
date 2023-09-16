75aac216efb133c4fb0dbf8636d0ba63dc15ba0f
KIMSEI1124 <workju1124@gmail.com>
2023-09-16 11:18:28 +0900

    Feat: Exception ErrorCode 수정

--- src/main/java/com/ssafy/tott/global/exception/dto/response/ExceptionResponse.java
+++ src/main/java/com/ssafy/tott/global/exception/dto/response/ExceptionResponse.java
@@ -2,19 +2,18 @@ package com.ssafy.tott.global.exception.dto.response;
 
 import com.ssafy.tott.global.exception.TOTTException;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 @Data
 public class ExceptionResponse {
     private String message;
-
-    private ExceptionResponse(String message) {
-        this.message = message;
-    }
+    private String errorCode;
 
     public static ExceptionResponse from(TOTTException e) {
-        return new ExceptionResponse(e.getMessage());
+        return new ExceptionResponse(e.getMessage(), e.getErrorCode());
     }
 }
